9d8526db93b7ddc9cefeb178c6efcfb59a62c3bb
Ged-sudo <gafter921@yandex.ru>
2023-05-13 13:10:55 +0300

    init

--- Struct/main.c
+++ Struct/main.c
@@ -2,12 +2,13 @@
 #include <stdlib.h>
 #include "node.h"
 
-int main(int argc, const char * argv[]) {    
+int main(int argc, const char * argv[]) {
     Node a = {3}, b = {17}, c = {21}, t = {10};
     Node * list = &a;
     a.next = &b;
     b.next = &c;
     c.next = NULL;
+    printf("");
    
     return 0;
 }
