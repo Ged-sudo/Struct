c0ff15c189945bc2741a0e001db8722edb98aec7
Ged-sudo <gafter921@yandex.ru>
2023-05-13 13:55:46 +0300

    test push func

--- Struct/main.c
+++ Struct/main.c
@@ -3,16 +3,13 @@
 #include "node.h"
 
 int main(int argc, const char * argv[]) {
-    Node a = {3}, b = {17}, c = {21}, t = {10};
-    Node * list = &a;
-    a.next = &b;
-    b.next = &c;
-    c.next = NULL;
-
+    Data test[] = {21, 17, 10};
+    Node * list  = NULL;
     
+    for (size_t i = 0; i < sizeof(test)/sizeof(test[0]); i++) {
+        push(&list, test[i]);
+    }
     
-    printList(list);
-    push(&list, 10);
     printList(list);
     printf("\n");
     
