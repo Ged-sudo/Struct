07d5cfa520edec5cb868f7deaf0952fce91e089c
Ged-sudo <gafter921@yandex.ru>
2023-05-14 11:04:43 +0300

    add func

--- Struct/main.c
+++ Struct/main.c
@@ -7,24 +7,34 @@ int main(int argc, const char * argv[]) {
     Data test[] = {21, 17, 10};
     Node * list  = NULL;
     
-    printf("Empty : %s\n", is_empty(list) ? "YES" : "NO");
-    
+//    printf("Empty : %s\n", is_empty(list) ? "YES" : "NO");
+//    printList(list);
+
     for (size_t i = 0; i < sizeof(test)/sizeof(test[0]); i++) {
         push(&list, test[i]);
     }
-    
+
+    printf("\n");
     printList(list);
     
-    printf("Empty : %s\n", is_empty(list) ? "YES" : "NO");
+    printf("%d \n",getLast(list)->data);
+    printf("%d \n", getNthelement(list, 1)->data);
+    pushBack(list, 11);
+    printList(list);
+    getLastButOne(list);
+    popBack(&list);
+    printList(list);
     
-    while (!is_empty(list)) {
-        Data d = pop(&list);
-        printf("pop %d : ", d);
-        printList(list);
-    }
-    
-    printf("Empty : %s\n", is_empty(list) ? "YES" : "NO");
-    printf("\n");
+//    printf("Empty : %s\n\n", is_empty(list) ? "YES" : "NO");
+//
+//    while (!is_empty(list)) {
+//        Data d = pop(&list);
+//        printf("pop %d : ", d);
+//        printList(list);
+//    }
+//
+//    printf("Empty : %s\n\n", is_empty(list) ? "YES" : "NO");
+//    printf("\n");
 
     return 0;
 }
--- Struct/node.h
+++ Struct/node.h
@@ -3,7 +3,7 @@ typedef struct Node Node;
 
 // Struct Item of list
 struct Node {
-    Data data;
+    int data;
     Node * next;
 };
 
@@ -12,22 +12,29 @@ typedef struct {
     Node * head;
 } Query;
 
-Node * list = NULL;
-
 void push(Node ** plist, Data x);
 Data pop(Node ** list);
 void printList(Node * list);
 int is_empty(Node * list);
+Node* getLast(Node *list);
+Node* getNthelement(Node* list, int n);
+Node* getLastButOne(Node* list);
+void popBack(Node **list);
 
-//print elements from list pointer
+
+//print elements from list pointer вывести все элемементы списка
 void printList(Node * list) {
-    for (Node * p = list; p!= NULL; p = p->next) {
-        printf("%d, ", p->data);
+    if (is_empty(list)) {
+        printf("List is empty.");
+    } else {
+        for (Node * p = list; p!= NULL; p = p->next) {
+            printf("%d, ", p->data);
+        }
+        printf("\n");
     }
-    printf("\n");
 }
 
-//add item at first
+//add item at first добавить элемент в начало списка
 void push(Node ** plist, Data value) {
     Node * p = malloc(sizeof(Node));
     p->data = value;
@@ -35,16 +42,87 @@ void push(Node ** plist, Data value) {
     *plist = p;
 }
 
-//check empty
+//check empty проверка на пустоту
 int is_empty(Node * list) {
     return list == NULL;
 }
 
-
+// del element удаление элемента
 Data pop(Node ** plist) {
-    Node * p = *plist;
-    Data res = p->data;
-    *plist = p->next;
+    Node * p = NULL;
+    Data val;
+    if (plist == NULL) {
+        exit(-1);
+    }
+    p = (*plist);
+    val = p->data;
+    (*plist) = (*plist)->next;
     free(p);
-    return res;
+    return val;
+}
+
+Node* getLast(Node *list) {
+    if (list == NULL) {
+        return NULL;
+    }
+    while (list->next) {
+        list = list->next;
+    }
+    return list;
+}
+
+Node* getNthelement(Node* list, int n) {
+    int counter = 0;
+    while (counter < n && list) {
+        if (list->next != NULL) {
+            list = list->next;
+            counter++;
+        } else {
+            printf("Error in count elements\n");
+            exit(-1);
+        }
+    }
+    return list;
+}
+
+void pushBack(Node *list, Data value) {
+    Node *last = getLast(list);
+    Node *tmp = (Node*) malloc(sizeof(Node));
+    tmp->data = value;
+    tmp->next = NULL;
+    last->next = tmp;
+}
+
+Node* getLastButOne(Node* list) {
+    if (list == NULL) {
+        exit(-2);
+    }
+    if (list->next == NULL) {
+        return NULL;
+    }
+    while (list->next->next) {
+        list = list->next;
+    }
+    return list;
+}
+
+void popBack(Node **list) {
+    Node *lastbn = NULL;
+    //Получили NULL
+    if (!list) {
+        exit(-1);
+    }
+    //Список пуст
+    if (!(*list)) {
+        exit(-1);
+    }
+    lastbn = getLastButOne(*list);
+    //Если в списке один элемент
+    if (lastbn == NULL) {
+        free(*list);
+        *list = NULL;
+    } else {
+        free(lastbn->next);
+        lastbn->next = NULL;
+    }
 }
